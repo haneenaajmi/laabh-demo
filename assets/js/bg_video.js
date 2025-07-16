2d6bccc0127c65ac65ff6bbfe6219cbbf9ab0ea9
Haneena Ajmi <149942521+haneenaajmi@users.noreply.github.com>
2025-07-16 13:36:08 +0400

    bg changed

--- assets/js/bg_video.js
+++ assets/js/bg_video.js
@@ -1,7 +1,24 @@
 // Ensure video plays properly on mobile devices
+// document.addEventListener('DOMContentLoaded', function () {
+//     const video = document.getElementById('video-background');
+
+// Try to play video (required for some mobile browsers)
+// Force autoplay (but don't show controls on failure)
+//     video.play().catch(() => { });
+// });
+
+
+// Ensure video plays properly on mobile devices
 document.addEventListener('DOMContentLoaded', function () {
     const video = document.getElementById('video-background');
+    console.log(video);
 
+    setTimeout(() => {
+
+        video.play().catch(() => {
+            console.log('video not playing');
+        });
+    }, 1000);
     // Try to play video (required for some mobile browsers)
     // Force autoplay (but don't show controls on failure)
     // video.play().catch(() => { });
